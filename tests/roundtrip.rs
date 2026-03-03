252abe49371bb5a3544a2b2cebabc98f520c5f62
Joel Bonini <joel.bonini@gmx.ch>
2026-03-03 14:11:04 +0100

    added tests for all 256 bytes and two unique bytes

--- tests/roundtrip.rs
+++ tests/roundtrip.rs
@@ -1,10 +1,40 @@
 use hydrogen::huffman;
 
-#[test]
-fn roundtrip_text() {
-    let input = b"hello world";
+fn assert_roundtrip(input: &[u8]) {
     let char_freq = huffman::get_freq(input);
     let (compressed, padding) = huffman::compress(input, &char_freq);
     let output = huffman::decode(padding, &char_freq, compressed);
     assert_eq!(input.to_vec(), output);
+}
+
+
+#[test]
+fn roundtrip_text() {
+    assert_roundtrip(b"hello world");
+}
+
+#[test]
+fn roundtrip_single_byte() {
+    assert_roundtrip(b"a");
+}
+
+#[test]
+fn roundtrip_repeated_byte() {
+    assert_roundtrip(b"aaaaaaa");
+}
+
+#[test]
+fn roundtrip_empty() {
+    assert_roundtrip(b"");
+}
+
+#[test]
+fn roundtrip_all_256_bytes() {
+    let input: Vec<u8> = (0..=255).collect();
+    assert_roundtrip(&input);
+}
+
+#[test]
+fn roundtrip_two_unique() {
+    assert_roundtrip(b"ababab");
 }
